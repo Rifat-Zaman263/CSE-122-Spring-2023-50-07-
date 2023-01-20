80ad014bd5a786ff672802f9a33c0a43a90892b9
Rifat Zaman <gamerrifat483@gmail.com>
2023-01-20 20:10:02 +0600

    Add files via upload

---
+++ Lab-2/04_263_50_07.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j,Count=0;
+   cin>>n>>j;
+   int x[100];
+   for(i=0;i<n;i++)
+   {
+     cin>>x[i];
+
+   }
+    for(i=0;i<n;i++)
+    {
+        if(x[i]>0 && x[i]>=x[j-1])
+        Count++;}
+   cout<<Count++;
+return 0;
+}
